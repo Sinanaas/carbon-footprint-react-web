82dc98a93d67b161474fcf667750092cd27dc9fc
M Sinan Abdul Syakur <84369115+Sinanaas@users.noreply.github.com>
2023-09-06 22:45:46 +0700

    Calculator done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Topbar from './Components/Topbar.js'
 import ArticlePictureSection from './Components/ArticlePictureSection';
-
+import PropaneCalc from './Components/PropaneCalc';
+import ElectricityCalc from './Components/ElectricityCalc';
+import WasteCalc from './Components/WasteCalc';
+import FuelCalc from './Components/FuelCalc';
 
 function App() {
   return (
@@ -10,8 +13,14 @@ function App() {
       <div className="topbar">
         <Topbar/>
       </div>
-      <div className="article-picture">
+      {/* <div className="article-picture">
         <ArticlePictureSection/>
+      </div> */}
+      <div className="calc-section">
+        <PropaneCalc/>
+        <ElectricityCalc/>
+        <WasteCalc/>
+        <FuelCalc/>
       </div>
     </div>
   );
---
+++ src/Components/ElectricityCalc.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+function ElectricityCalc() {
+  const [kWhUsed, setkWhUsed] = useState('');
+  const [results, setResults] = useState(null);
+
+  function calculateElectricityEmissions(kWhUsed) {
+    const lbsCO2PerMWhGenerated = 884.2; // lbs of CO2 per megawatt-hour generated
+    const MWhDeliveredToGeneratedRatio = 1 / (1 - 0.073); // Ratio of MWh delivered to MWh generated
+    const kWhToMWhConversion = 1 / 1000; // Convert kWh to MWh
+    const lbToMetricTonConversion = 1 / 2204.6; // Convert lbs to metric tons
+  
+    const metricTonsCO2 = kWhUsed * lbsCO2PerMWhGenerated * MWhDeliveredToGeneratedRatio * kWhToMWhConversion * lbToMetricTonConversion;
+    return metricTonsCO2;
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const metricTonsCO2 = calculateElectricityEmissions(parseFloat(kWhUsed));
+    setResults(metricTonsCO2);
+  };
+
+  return (
+    <div className="ElectricityContainer">
+      <div className="electricity-header">
+        <h1>Electricity Consumption</h1>
+      </div>
+      <div className="electricity-form">
+        <form onSubmit={handleSubmit}>
+          <h3>Electricity consumption in a year (kWh)</h3>
+          <input
+            type="number"
+            placeholder="Enter kWh"
+            value={kWhUsed}
+            onChange={(e) => setkWhUsed(e.target.value)}
+            required
+          />
+          <button type="submit">Calculate</button>
+        </form>
+        {results !== null && (
+          <div className="electricity-results">
+            <h1>Carbon Emissions:</h1>
+            <p>{results} metric tons CO2</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ElectricityCalc;
